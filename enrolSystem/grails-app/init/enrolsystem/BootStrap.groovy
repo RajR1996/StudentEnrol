7e5a53f54b8528b819b4c2041bec82e4772ce8b2
RajR1996 <b4025068@my.shu.ac.uk>
2017-02-15 18:24:00 +0000

    Created Domain Class Module and Controller Class Module Controller

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -3,6 +3,8 @@ package enrolsystem
 class BootStrap {
 
     def init = { servletContext ->
+new Module(title: "Sport", code: "S", credits: "120", Lecturer: "Randomer", Description: 'this is physical education.').save()
+
     }
     def destroy = {
     }
