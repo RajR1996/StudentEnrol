093e1a2cd2e7809e4e9e62acfb8168b00be24c57
RajR1996 <b4025068@my.shu.ac.uk>
2017-02-16 06:00:02 +0000

    Lecturer Class and Controller

--- enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
+++ enrolSystem/grails-app/init/enrolsystem/BootStrap.groovy
@@ -1,9 +1,7 @@
 package enrolsystem
 
 class BootStrap {
-
     def init = { servletContext ->
-new Module(title: "Sport", code: "S", credits: "120", Lecturer: "Randomer", Description: 'this is physical education.').save()
 
     }
     def destroy = {
